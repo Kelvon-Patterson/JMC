3cd79b54babec069c157fc2546b85b3492b79e8e
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-22 13:04:22 -0500

    feaet:char and boolean

---
+++ src/CharAndBoolean.java
@@ -0,0 +1,9 @@
+public class CharAndBoolean {
+    
+   public static void main(String [] args){
+       char myChar = 'D';
+    char myUnicode = '\u0044';
+       System.out.println(myChar);
+       System.out.println(myUnicode);
+   }
+}
